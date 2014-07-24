578cf13589e2b15d5bcd0002e2bba57a0ebb42ef
Bart Bakker <bart@thesoftwarecraft.com>
2014-07-24 15:02:37 +0200

    mvn: preserve mvn exit status

--- .zsh/java.zsh
+++ .zsh/java.zsh
@@ -2,6 +2,6 @@ export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
 
 # Maven needs lots of -err- everything
 export MAVEN_OPTS='-Xms128m -Xmx512m'
-function mvn() { /usr/local/bin/mvn $* | tee /tmp/last-mvn ; }
+function mvn() { /usr/local/bin/mvn $* | tee /tmp/last-mvn ; return $pipestatus[1] }
 alias last-mvn='less +k /tmp/last-mvn'
 
