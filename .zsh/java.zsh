bf40426e01fe87213ef1b564cd5aa592a5d99131
Bart Bakker <bart@thesoftwarecraft.com>
2014-09-08 11:56:57 +0200

    Maven takes at least 64m of ram

--- .zsh/java.zsh
+++ .zsh/java.zsh
@@ -1,7 +1,7 @@
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 
 # Maven needs lots of -err- everything
-export MAVEN_OPTS='-Xms128m -Xmx512m'
+export MAVEN_OPTS='-Xms64m -Xmx512m'
 function mvn() { /usr/local/bin/mvn $* | tee /tmp/last-mvn ; return $pipestatus[1] }
 alias last-mvn='less +k /tmp/last-mvn'
 
