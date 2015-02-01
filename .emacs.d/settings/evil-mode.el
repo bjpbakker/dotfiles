2d0d878f8972c6fadd128da6d8f9020cef7e1e85
Bart Bakker <bart@thesoftwarecraft.com>
2015-02-01 13:55:49 +0100

    some evil keybindings

--- .emacs.d/settings/evil-mode.el
+++ .emacs.d/settings/evil-mode.el
@@ -6,14 +6,14 @@
 (global-evil-leader-mode)
 (evil-mode 1)
 
-(setq evil-jumper-auto-center t)
-(global-evil-jumper-mode t)
-
 (setq evil-leader/leader ",")
 (evil-leader/set-key
   "f" 'projectile-find-file
   "b" 'switch-to-buffer
   "cd" 'cd)
+
+(setq evil-jumper-auto-center t)
+(global-evil-jumper-mode t)
 
 (evil-commentary-mode)
 
@@ -29,3 +29,6 @@
   (define-key evil-normal-state-map key action))
 
 (nmap (kbd "RET") 'save-buffer)
+(nmap (kbd "C-b") 'evil-scroll-up)
+(nmap (kbd "C-f") 'evil-scroll-down)
+(nmap (kbd "Y") (kbd "y$"))
