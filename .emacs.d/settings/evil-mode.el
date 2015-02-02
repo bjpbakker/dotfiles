a35e2507067d57c8eae8695f7d5f4cbcc190f07d
Bart Bakker <bart@thesoftwarecraft.com>
2015-02-02 17:48:37 +0100

    extract key-bindings in separate config file

--- .emacs.d/settings/evil-mode.el
+++ .emacs.d/settings/evil-mode.el
@@ -6,47 +6,7 @@
 (global-evil-leader-mode)
 (evil-mode 1)
 
-(setq evil-leader/leader ",")
-(evil-leader/set-key
-  "f" 'projectile-find-file
-  "b" 'switch-to-buffer
-  "cd" 'cd
-  "h" (lambda () (interactive) (call-interactively 'help)))
-
 (setq evil-jumper-auto-center t)
 (global-evil-jumper-mode t)
 
 (evil-commentary-mode)
-
-; delete evil motion key bindings for RET and SPC. These keys are
-; useless motion-wise and should be mapped in normal mode.
-(defun delete-evil-motion-key! (key)
-  "Delete key binding from evil motion keymap"
-  (define-key evil-motion-state-map key nil))
-(delete-evil-motion-key! (kbd "RET"))
-(delete-evil-motion-key! (kbd " "))
-
-(defmacro bind (&rest commands)
-  "Convenience macro to bind to interactive functions"
-  `(lambda () (interactive) ,@commands))
-
-(defun nmap (key action)
-  (define-key evil-normal-state-map key action))
-(defun imap (key action)
-  (define-key evil-insert-state-map key action))
-
-(nmap (kbd "RET") 'save-buffer)
-(nmap (kbd "C-b") 'evil-scroll-up)
-(nmap (kbd "C-f") 'evil-scroll-down)
-(nmap (kbd "Y") (kbd "y$"))
-
-(nmap (kbd "C-h") 'evil-window-left)
-(nmap (kbd "C-l") 'evil-window-right)
-(nmap (kbd "C-k") 'evil-window-up)
-(nmap (kbd "C-j") 'evil-window-down)
-
-(imap (kbd "C-u") (bind (kill-line 0)))
-
-(require 'magit)
-(with-eval-after-load 'magit
-  (global-set-key (kbd "C-x g") 'magit-status))
