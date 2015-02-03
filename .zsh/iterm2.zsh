9fa755b2d4503e5f33b8f159c4487f99bc5422c8
Bart Bakker <bart@thesoftwarecraft.com>
2015-02-03 10:24:53 +0100

    use spaces for indentation

--- .zsh/iterm2.zsh
+++ .zsh/iterm2.zsh
@@ -1,22 +1,22 @@
 iterm2_precmd() {
-	local title=${PWD##*/}
-	_iterm2_update_title $title
+    local title=${PWD##*/}
+    _iterm2_update_title $title
 }
 
 iterm2_preexec() {
-	local title="Running: $1"
-	_iterm2_update_title $title
+    local title="Running: $1"
+    _iterm2_update_title $title
 }
 
 iterm2_user_title() {
     add-zsh-hook -d precmd iterm2_precmd
     add-zsh-hook -d preexec iterm2_preexec
-	_iterm2_update_title $*
+    _iterm2_update_title $*
 }
 
 _iterm2_update_title() {
-	local new_title=$1
-	echo -n "\e]1;$new_title\a"
+    local new_title=$1
+    echo -n "\e]1;$new_title\a"
 }
 
 add-zsh-hook precmd iterm2_precmd
