d7231a846ee71f5354747e5a34abe4cffa49e9f1
Bart Bakker <bart@thesoftwarecraft.com>
2015-02-03 11:37:50 +0100

    ruby migrate and remigrate aliases

--- .zsh/ruby.zsh
+++ .zsh/ruby.zsh
@@ -1,2 +1,4 @@
 eval "$(rbenv init -)"
 
+alias migrate='bundle exec rake db:migrate db:test:clone parallel:prepare'
+alias remigrate='bundle exec rake db:drop db:create db:migrate db:test:clone parallel:prepare'
