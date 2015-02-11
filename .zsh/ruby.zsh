53c84579250e9081f6e3bf044b3361d5912c51c0
Bart Bakker <bart@thesoftwarecraft.com>
2015-02-11 22:00:55 +0100

    ruby: use db:schema:load instead of db:migrate

--- .zsh/ruby.zsh
+++ .zsh/ruby.zsh
@@ -1,4 +1,4 @@
 eval "$(rbenv init -)"
 
-alias migrate='bundle exec rake db:migrate db:test:clone parallel:prepare'
-alias remigrate='bundle exec rake db:drop db:create db:migrate db:test:clone parallel:prepare'
+alias migrate='bundle exec rake db:schema:load db:test:clone parallel:prepare'
+alias remigrate='bundle exec rake db:drop db:create db:schema:load db:test:clone parallel:prepare'
