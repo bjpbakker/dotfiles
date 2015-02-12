e3115a67f8b4e32d09a1c4d47fa2a410a3d899ce
Bart Bakker <bart@thesoftwarecraft.com>
2015-02-12 23:49:55 +0100

    some sensible keybindings on neotree in evil mode

--- .emacs.d/settings/navigation.el
+++ .emacs.d/settings/navigation.el
@@ -20,4 +20,10 @@
 (setup 'uniquify
        (setq uniquify-buffer-name-style 'forward))
 
-(require 'neotree)
+(setup 'neotree
+       (add-hook 'neotree-mode-hook
+                 (lambda ()
+                   (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+                   (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+                   (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+                   (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter))))
