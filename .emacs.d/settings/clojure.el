f8dc182ec142565f4f8fa36bd3578ffccc44a6eb
Bart Bakker <bart@thesoftwarecraft.com>
2015-02-16 22:24:30 +0100

    set up clojure key-bindings to run tests

--- .emacs.d/settings/clojure.el
+++ .emacs.d/settings/clojure.el
@@ -4,4 +4,12 @@
        (add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode)))
 
 (setup 'cider
-       (add-hook 'clojure-mode-hook #'cider-mode))
+       (add-hook 'clojure-mode-hook #'cider-mode)
+       (add-hook 'cider-mode #'clojure/customize-cider))
+
+(defun clojure/customize-cider ()
+  (with-eval-after-load 'evil-leader
+    (evil-leader/set-key
+      "T" 'cider-test-run-test
+      "t" 'cider-test-run-tests
+      "r" 'cider-test-rerun-tests)))
