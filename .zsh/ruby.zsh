b3b5ea69ac0bf7865d66e64b235ea282bcf9e55f
Bart Bakker <bart@thesoftwarecraft.com>
2015-02-18 13:53:09 +0100

    ruby: limit number of CPUs for parallel specs

--- .zsh/ruby.zsh
+++ .zsh/ruby.zsh
@@ -2,3 +2,5 @@ eval "$(rbenv init -)"
 
 alias migrate='bundle exec rake db:schema:load db:test:clone parallel:prepare'
 alias remigrate='bundle exec rake db:drop db:create db:schema:load db:test:clone parallel:prepare'
+
+export PARALLEL_TEST_PROCESSORS=6
