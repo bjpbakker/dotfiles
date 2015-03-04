5be7288c9a37821d0dbe12ede601e184094f0088
Bart Bakker <bart@thesoftwarecraft.com>
2015-03-04 21:10:21 +0100

    ruby: remigrate uses db:setup (to include db:seed)

--- .zsh/ruby.zsh
+++ .zsh/ruby.zsh
@@ -3,6 +3,6 @@ eval "$(rbenv init -)"
 be() { bundle exec $* }
 
 alias migrate='be rake db:migrate db:test:clone parallel:prepare'
-alias remigrate='be rake db:drop db:create db:schema:load && be rake db:test:clone parallel:prepare'
+alias remigrate='be rake db:drop db:create db:setup && be rake db:test:clone parallel:prepare'
 
 export PARALLEL_TEST_PROCESSORS=6
