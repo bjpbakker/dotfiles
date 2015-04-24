9fa611a53c5879c08da9def6bd58f8bbde6ba45f
Bart Bakker <bart@thesoftwarecraft.com>
2015-04-24 20:53:53 +0200

    Update ruby db aliases to Rails 4.1
    Also rename `remigrate` to `db_reset` as it does not re-apply all migrations.

--- .zsh/ruby.zsh
+++ .zsh/ruby.zsh
@@ -2,7 +2,7 @@ eval "$(rbenv init -)"
 
 be() { bundle exec $* }
 
-alias migrate='be rake db:migrate db:test:clone parallel:prepare'
-alias remigrate='be rake db:drop db:create db:setup && be rake db:test:clone parallel:prepare'
+alias migrate='be rake db:migrate parallel:prepare'
+alias db_reset='be rake db:drop db:create db:setup && be rake parallel:prepare'
 
 export PARALLEL_TEST_PROCESSORS=6
