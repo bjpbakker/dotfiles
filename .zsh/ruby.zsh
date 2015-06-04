6661449753774bda40d03a0a82091bdb0e0ec48d
Bart Bakker <bart@thesoftwarecraft.com>
2015-06-04 10:08:43 +0200

    Ruby `pspec` alias to run specs in parallel w/o Spring

--- .zsh/ruby.zsh
+++ .zsh/ruby.zsh
@@ -4,5 +4,6 @@ be() { bundle exec $* }
 
 alias migrate='be rake db:migrate parallel:prepare'
 alias db_reset='be rake db:drop db:create db:setup && be rake parallel:prepare'
+alias pspec='DISABLE_SPRING=1 be rake spec'
 
 export PARALLEL_TEST_PROCESSORS=6
