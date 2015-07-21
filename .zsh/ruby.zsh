58953da1977b3ce572414cf0d22c38e4e1b9c91c
Bart Bakker <bart@thesoftwarecraft.com>
2015-07-21 17:04:31 +0200

    Ruby: parallel:prepare is not required for migrate

--- .zsh/ruby.zsh
+++ .zsh/ruby.zsh
@@ -2,7 +2,7 @@ eval "$(rbenv init -)"
 
 be() { bundle exec $* }
 
-alias migrate='be rake db:migrate parallel:prepare'
+alias migrate='be rake db:migrate'
 alias db_reset='be rake db:drop db:create db:setup && be rake parallel:prepare'
 alias pspec='DISABLE_SPRING=1 be rake spec'
 
