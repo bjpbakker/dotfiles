65bb9a7c9263f41c1e0b7964e4f565cc02065982
Bart Bakker <bart@thesoftwarecraft.com>
2015-10-14 23:20:38 +0200

    Improve neotree configuration

--- .emacs.d/settings/navigation.el
+++ .emacs.d/settings/navigation.el
@@ -1,32 +1,35 @@
 (require 'config-util)
 
 (setup 'projectile
-       (projectile-global-mode))
+  (projectile-global-mode))
 
 (setup 'ido
-       (setq ido-enable-prefix nil
-             ido-enable-flex-matching t
-             ido-use-virtual-buffers t
-             ido-use-faces nil)
-       (ido-mode +1))
+  (setq ido-enable-prefix nil
+        ido-enable-flex-matching t
+        ido-use-virtual-buffers t
+        ido-use-faces nil)
+  (ido-mode +1))
 (setup 'flx-ido
-       (flx-ido-mode +1))
+  (flx-ido-mode +1))
 (setup 'ido-vertical-mode
-       (ido-vertical-mode 1))
+  (ido-vertical-mode 1))
 
 ;; if a file is changed outside of Emacs, reload without asking
 (global-auto-revert-mode t)
 
 (setup 'uniquify
-       (setq uniquify-buffer-name-style 'forward))
+  (setq uniquify-buffer-name-style 'forward))
 
 (setup 'neotree
-       (add-hook 'neotree-mode-hook
-                 (lambda ()
-                   (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
-                   (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
-                   (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
-                   (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+  (defun neo-change-to-project-root ()
+    (neo-global--with-buffer
+      (neo-buffer--change-root (projectile-project-root))))
+  (setq neo-vc-integration '(char)
+        neo-theme 'arrow
+        neo-mode-line-type 'none
+        neo-smart-open t
+        neo-dont-be-alone t)
+  (advice-add 'neotree-show :after #'neo-change-to-project-root)
 
-       (with-eval-after-load 'evil
-         (add-to-list 'evil-emacs-state-modes 'neotree-mode)))
+  (with-eval-after-load 'evil
+    (add-to-list 'evil-emacs-state-modes #'neotree-mode)))
