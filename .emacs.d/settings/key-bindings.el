07a55241e7835c1fee469d7e227204659ba44495
Bart Bakker <bart@thesoftwarecraft.com>
2015-11-11 00:32:43 +0100

    Drop evil leader navigation key bindings

--- .emacs.d/settings/key-bindings.el
+++ .emacs.d/settings/key-bindings.el
@@ -40,9 +40,6 @@
 (with-eval-after-load 'evil-leader
   (setq evil-leader/leader ",")
   (evil-leader/set-key
-    "f" 'helm-projectile-find-file
-    "b" 'switch-to-buffer
-    "cd" 'helm-projectile-switch-project
     "h" (bind (call-interactively 'help))
     "." 'projectile-toggle-between-implementation-and-test))
 
