5d6afb1967d99711e5e43b8b62390232a8ce1411
Bart Bakker <bart@thesoftwarecraft.com>
2015-12-28 17:56:35 +0100

    Make company-dabbrev case sensitive

--- .emacs.d/settings/setup-company.el
+++ .emacs.d/settings/setup-company.el
@@ -3,7 +3,9 @@
 
 (setq company-idle-delay 0.2
       company-tooltip-limit 10
-      company-tooltip-flip-when-above t)
+      company-tooltip-flip-when-above t
+      company-dabbrev-downcase nil
+      company-dabbrev-ignore-case t)
 
 (add-hook 'after-init-hook 'global-company-mode)
 (add-hook 'company-mode-hook 'company-flx-mode)
