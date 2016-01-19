2ae2ef4a2b13a58645b3adc413e7a21ab7067620
Bart Bakker <bart@thesoftwarecraft.com>
2016-01-19 21:59:18 +0100

    No longer set magit-push-always-verify
    Magit has removed this variable, clean it from customizations.

--- .emacs.d/settings/setup-magit.el
+++ .emacs.d/settings/setup-magit.el
@@ -1,6 +1,5 @@
 (require 'magit)
-(setq magit-push-always-verify nil
-      magit-no-confirm (list 'discard 'revert 'stage-all-changes 'unstage-all-changes))
+(setq magit-no-confirm '(discard revert stage-all-changes unstage-all-changes))
 
 (with-eval-after-load 'evil
   (add-to-list 'evil-emacs-state-modes 'magit-mode))
