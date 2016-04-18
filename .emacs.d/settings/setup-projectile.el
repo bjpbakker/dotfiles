10e230e3e55fb9d5dcecd0a7e67e2b74d8d66481
Bart Bakker <bart@thesoftwarecraft.com>
2016-04-18 22:23:19 +0200

    Delete some left-over helm config

--- .emacs.d/settings/setup-projectile.el
+++ .emacs.d/settings/setup-projectile.el
@@ -1,9 +1,4 @@
 (require 'projectile)
 (projectile-global-mode)
 
-(with-eval-after-load 'helm
-  (require 'helm-projectile)
-  (setq projectile-completion-system 'helm)
-  (helm-projectile-on))
-
 (provide 'setup-projectile)
