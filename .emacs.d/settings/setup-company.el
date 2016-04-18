93339edcdd8e42e443ffe3cf9fcca0ec782d2c67
Bart Bakker <bart@thesoftwarecraft.com>
2016-04-18 22:23:39 +0200

    Never show company after some idle timeout
    Use <C-return> to complete with company mode but never auto show the
    popup. It's annoying.

--- .emacs.d/settings/setup-company.el
+++ .emacs.d/settings/setup-company.el
@@ -1,7 +1,7 @@
 (require 'company)
 (require 'company-flx)
 
-(setq company-idle-delay 0.5
+(setq company-idle-delay nil
       company-tooltip-limit 10
       company-tooltip-flip-when-above t
       company-dabbrev-downcase nil
