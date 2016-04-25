818d03a38326f18ca752fe99ed80446987d46fcc
Bart Bakker <bart@thesoftwarecraft.com>
2016-04-25 20:55:19 +0200

    Use Ivy specifically for projectile search and M-x

--- .emacs.d/settings/setup-ivy.el
+++ .emacs.d/settings/setup-ivy.el
@@ -3,8 +3,6 @@
 (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy))
       ivy-initial-inputs-alist nil)
 
-(global-set-key (kbd "M-x") 'counsel-M-x)
-
 (set-face-attribute 'ivy-current-match nil
                     :background "#ffc911"
                     :foreground "#1c1c1c")
@@ -12,6 +10,6 @@
 (with-eval-after-load 'projectile
   (setq projectile-completion-system 'ivy))
 
-(ivy-mode)
+(global-set-key (kbd "M-x") 'counsel-M-x)
 
 (provide 'setup-ivy)
