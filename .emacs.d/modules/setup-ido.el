771bac1ed3da00377f57f887781e221eba9e1c09
Bart Bakker <bart@thesoftwarecraft.com>
2016-05-11 15:27:51 +0200

    Single `setq` in setup-ido

--- .emacs.d/modules/setup-ido.el
+++ .emacs.d/modules/setup-ido.el
@@ -7,18 +7,16 @@
       ido-enable-flex-matching t
       ido-create-new-buffer 'always
       ido-use-filename-at-point 'guess
-      ido-max-prospects 10
-      ido-save-directory-list-file (expand-file-name "ido.hist" emacs-temp-directory)
       ido-default-file-method 'selected-window
+      ido-max-prospects 10
+      ido-use-faces nil ;; see flx highlights
+      ido-vertical-define-keys 'C-n-and-C-p-only
+      ido-save-directory-list-file (expand-file-name "ido.hist" emacs-temp-directory)
       ido-auto-merge-work-directories-length -1)
 
 (ido-mode t)
 (ido-ubiquitous-mode t)
-
 (flx-ido-mode t)
-(setq ido-use-faces nil) ;; show flx highlights
-
 (ido-vertical-mode t)
-(setq ido-vertical-define-keys 'C-n-and-C-p-only)
 
 (provide 'setup-ido)
