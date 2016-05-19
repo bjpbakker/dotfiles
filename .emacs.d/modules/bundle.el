ecd2bfafe63ef9efe9d03f79c027576d6220ecb0
Bart Bakker <bart@thesoftwarecraft.com>
2016-05-19 12:59:08 +0200

    Restructure `bundle.el`

--- .emacs.d/modules/bundle.el
+++ .emacs.d/modules/bundle.el
@@ -10,33 +10,82 @@
   "Packages required at runtime")
 
 (setq bart/packages
-      '(flx-ido ido-ubiquitous ido-vertical-mode ;; fuzzy match files, buffers, and more
-        ivy swiper counsel counsel-projectile flx ;; Ivy search and fuzzy matching
-        projectile ;; open projects, files and more
-        neotree etags-select ;; navigation
-        undo-tree avy expand-region embrace fillcode paredit rainbow-delimiters toggle-quotes idle-highlight-mode ;; editing
-        ace-window ;; window control
-        company company-flx ;; completion
-        flycheck flycheck-tip ;; syntax checking
-        editorconfig ;; project-based configuration
-        which-key discover-my-major ;; more help
-        smart-mode-line column-enforce-mode ;; ui enhancements
-        rainbow-mode ;; display color strings
-        clojure-mode cider ;; clojure
-        elixir-mode ;; elixir
-        haskell-mode ;; haskell
-        js2-mode ;; javascript
-        markdown-mode writegood-mode ;; markdown
-        purescript-mode flycheck-purescript psci ;; purescript
-        ruby-end inf-ruby rbenv bundler rspec-mode projectile-rails goto-gem ruby-refactor ruby-hash-syntax ;; ruby
-        rust-mode flycheck-rust racer cargo company-racer ;; rust
-        yaml-mode ;; yaml
-        swift-mode company-sourcekit ;; swift
-        web-mode less-css-mode haml-mode sass-mode ;; web support
-        realgud ;; debugging
-        magit ag dash-at-point ;; tools
-        org deft ;; org-mode tools
-        alect-themes zerodark-theme select-themes)) ;; color themes
+      '(
+        ;; fuzzy match files, buffers, and more
+        flx-ido ido-ubiquitous ido-vertical-mode
+        ivy swiper counsel counsel-projectile flx
+
+        ;; projects
+        projectile editorconfig neotree etags-select
+
+        ;; editing
+        undo-tree expand-region
+        fillcode paredit embrace toggle-quotes
+        avy rainbow-delimiters
+        rainbow-delimiters idle-highlight-mode rainbow-mode
+
+        ;; window movement
+        ace-window
+
+        ;; completion
+        company company-flx
+
+        ;; syntax checking
+        flycheck flycheck-tip
+
+        ;; more help
+        which-key discover-my-major
+
+        ;; ui enhancements
+        smart-mode-line column-enforce-mode
+
+        ;; color themes
+        select-themes
+        alect-themes ample-theme minimal-theme leuven-theme
+
+        ;; clojure
+        clojure-mode cider
+
+        ;; haskell
+        haskell-mode flycheck-stack
+
+        ;; javascript
+        js2-mode
+
+        ;; markdown
+        markdown-mode writegood-mode
+
+        ;; purescript
+        purescript-mode flycheck-purescript psci
+
+        ;; ruby
+        ruby-end inf-ruby rbenv bundler rspec-mode projectile-rails
+        goto-gem ruby-refactor ruby-hash-syntax
+
+        ;; rust
+        rust-mode flycheck-rust racer cargo company-racer
+
+        ;; yaml
+        yaml-mode
+
+        ;; swift
+        swift-mode company-sourcekit
+
+        ;; html, css (and preprocessors), and multi mode
+        web-mode less-css-mode haml-mode sass-mode
+
+        ;; debugging
+        realgud
+
+        ;; git
+        magit
+
+        ;; org
+        org deft
+
+        ;; misc tools
+        ag dash-at-point
+))
 
 (defun install-missing-packages ()
   (interactive)
