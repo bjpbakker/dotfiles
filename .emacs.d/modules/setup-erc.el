144d9f3c5d912299f43eb1fe83f9bb64d39907bb
Bart Bakker <bart@thesoftwarecraft.com>
2016-06-10 21:52:21 +0200

    Add freenode IRC configuration

--- .emacs.d/modules/setup-erc.el
+++ .emacs.d/modules/setup-erc.el
@@ -1,23 +1,35 @@
 (require 'erc)
+(require 'erc-services)
 
 (setq erc-user-full-name "Bart Bakker"
       erc-interpret-mirc-color t
       erc-kill-buffer-on-part t
       erc-kill-queries-on-quit t
       erc-kill-server-buffer-on-quit t
-      erc-query-display 'buffer)
+      erc-query-display 'buffer
+      erc-prompt-for-nickserv-password nil)
 
 (erc-truncate-mode t)
 (erc-spelling-mode t)
+(erc-services-mode t)
 
 (let ((erc-passwords-file (expand-file-name "~/.ercpass")))
   (when (file-exists-p erc-passwords-file)
     (load erc-passwords-file)))
+
+(setq erc-nickserv-passwords
+      `((freenode (("bjpbakker" . ,freenode-nickserv-password)))))
 
 (defun erc-connect/slack ()
   "Connect to Slack"
   (interactive)
   (erc-tls :server "trythisjob.irc.slack.com" :port 6667
            :nick "bjpbakker" :password slack-connect-password))
+
+(defun erc-connect/freenode ()
+  "Connect to Freenode IRC"
+  (interactive)
+  (erc-tls :server "irc.freenode.net" :port 6697
+           :nick "bjpbakker" :full-name "Bart"))
 
 (provide 'setup-erc)
