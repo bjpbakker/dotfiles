846bd8d41e819bc4b64435c44742456e458ee6d1
Bart Bakker <bart@thesoftwarecraft.com>
2016-06-30 13:49:30 +0200

    Delete 'flow-types for the time being
    While very useful when intensively using flow types, it's in the way for
    standard javascript.
    
    This needs a separate flowtype-mode.

--- .emacs.d/languages/javascript.el
+++ .emacs.d/languages/javascript.el
@@ -25,7 +25,6 @@
   (local-set-key (kbd "C-c , r") #'mocha-verify-last))
 
 (defun setup-js-flycheck ()
-  (require 'flow-types)
   (with-eval-after-load 'flycheck
     (require 'flycheck-javascript-flow)
     (add-to-list 'flycheck-checkers 'javascript-flow)))
